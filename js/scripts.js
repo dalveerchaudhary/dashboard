1f658534e81a782b45637b7027e1821767577c6b
dalveersingh <dalveerchu1@gmail.com>
2024-09-19 23:30:49 +0530

    small changes for all pages

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,11 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});  
